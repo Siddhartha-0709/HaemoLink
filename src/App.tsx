f8fe4d1e94c137815ecdc21e82ec57aa7854508b
Siddhartha Mukherjee <siddharthamukherjee0709@gmail.com>
2023-08-05 19:33:18 +0530

    changed folder structure

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>
+        Hello Native
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
